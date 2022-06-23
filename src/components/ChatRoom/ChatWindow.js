41e91084f6c06135a39f25ceba67e2fbe9dd7b19
hoangnhat42 <hoangnhat.contact@gmail.com>
2022-06-23 23:43:00 +0700

    ChatWindow

--- src/components/ChatRoom/ChatWindow.js
+++ src/components/ChatRoom/ChatWindow.js
@@ -71,47 +71,128 @@ const MessageListStyled = styled.div`
 `;
 
 export default function ChatWindow() {
+  const { selectedRoom, members, setIsInviteMemberVisible } =
+    useContext(AppContext);
+  const {
+    user: { uid, photoURL, displayName },
+  } = useContext(AuthContext);
+  const [inputValue, setInputValue] = useState('');
+  const [form] = Form.useForm();
+  const inputRef = useRef(null);
+  const messageListRef = useRef(null);
 
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
-    return (
-        <WrapperStyled>
+  const handleOnSubmit = () => {
+    addDocument('messages', {
+      text: inputValue,
+      uid,
+      photoURL,
+      roomId: selectedRoom.id,
+      displayName,
+    });
 
-              <HeaderStyled>
-                <div className='header__info'>
-                  <p className='header__title'></p>
-                  <span className='header__description'>
-            
-                  </span>
-                </div>
-                <ButtonGroupStyled>
-                  <Button
-                    icon={<UserAddOutlined />}
-                    type='text'
-                  >
-                    Mời
-                  </Button>
-                  <Avatar.Group size='small' maxCount={2}>
+    form.resetFields(['message']);
 
-                  </Avatar.Group>
-                </ButtonGroupStyled>
-              </HeaderStyled>
-              <ContentStyled>
-
-                <FormStyled>
-                  <Form.Item name='message'>
-                    <Input
-                     
-                      placeholder='Nhập tin nhắn...'
-                      bordered={false}
-                      autoComplete='off'
-                    />
-                  </Form.Item>
-                  <Button type='primary'>
-                    Gửi
-                  </Button>
-                </FormStyled>
-              </ContentStyled>
-
-        </WrapperStyled>
-      );
+    // focus to input again after submit
+    if (inputRef?.current) {
+      setTimeout(() => {
+        inputRef.current.focus();
+      });
     }
+  };
+
+  const condition = React.useMemo(
+    () => ({
+      fieldName: 'roomId',
+      operator: '==',
+      compareValue: selectedRoom.id,
+    }),
+    [selectedRoom.id]
+  );
+
+  const messages = useFirestore('messages', condition);
+
+  useEffect(() => {
+    // scroll to bottom after message changed
+    if (messageListRef?.current) {
+      messageListRef.current.scrollTop =
+        messageListRef.current.scrollHeight + 50;
+    }
+  }, [messages]);
+
+  return (
+    <WrapperStyled>
+      {selectedRoom.id ? (
+        <>
+          <HeaderStyled>
+            <div className='header__info'>
+              <p className='header__title'>{selectedRoom.name}</p>
+              <span className='header__description'>
+                {selectedRoom.description}
+              </span>
+            </div>
+            <ButtonGroupStyled>
+              <Button
+                icon={<UserAddOutlined />}
+                type='text'
+                onClick={() => setIsInviteMemberVisible(true)}
+              >
+                Mời
+              </Button>
+              <Avatar.Group size='small' maxCount={2}>
+                {members.map((member) => (
+                  <Tooltip title={member.displayName} key={member.id}>
+                    <Avatar src={member.photoURL}>
+                      {member.photoURL
+                        ? ''
+                        : member.displayName?.charAt(0)?.toUpperCase()}
+                    </Avatar>
+                  </Tooltip>
+                ))}
+              </Avatar.Group>
+            </ButtonGroupStyled>
+          </HeaderStyled>
+          <ContentStyled>
+            <MessageListStyled ref={messageListRef}>
+              {messages.map((mes) => (
+                <Message
+                  key={mes.id}
+                  text={mes.text}
+                  photoURL={mes.photoURL}
+                  displayName={mes.displayName}
+                  createdAt={mes.createdAt}
+                />
+              ))}
+            </MessageListStyled>
+            <FormStyled form={form}>
+              <Form.Item name='message'>
+                <Input
+                  ref={inputRef}
+                  onChange={handleInputChange}
+                  onPressEnter={handleOnSubmit}
+                  placeholder='Nhập tin nhắn...'
+                  bordered={false}
+                  autoComplete='off'
+                />
+              </Form.Item>
+              <Button type='primary' onClick={handleOnSubmit}>
+                Gửi
+              </Button>
+            </FormStyled>
+          </ContentStyled>
+        </>
+      ) : (
+        <Alert
+          message='Hãy chọn phòng'
+          type='info'
+          showIcon
+          style={{ margin: 5 }}
+          closable
+        />
+      )}
+    </WrapperStyled>
+  );
+}
